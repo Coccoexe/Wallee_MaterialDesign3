cd9aac6ed665d4504c5493dd46c8af185ba1a884
Coccoexe <74604365+Coccoexe@users.noreply.github.com>
2022-05-24 10:24:05 +0200

    first commit

---
+++ app/src/main/java/com/example/md3/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.md3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.color.DynamicColors
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        DynamicColors.applyToActivityIfAvailable(this)
+    }
+}
