7574347dae8d36046048ad5fbf01eb00caaa05d0
Coccoexe <74604365+Coccoexe@users.noreply.github.com>
2022-05-28 11:45:54 +0200

    add cancel button

--- app/src/main/java/com/example/md3/MainActivity.kt
+++ app/src/main/java/com/example/md3/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.md3
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.Spinner
-import android.widget.SpinnerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.color.DynamicColors
 
@@ -16,11 +13,9 @@ class MainActivity : AppCompatActivity() {
 
         val addTrans : Button = findViewById(R.id.addTransaction)
         addTrans.setOnClickListener{
-            var popup = addTransPopup()
+            val popup = addTransPopup()
 
             popup.show(supportFragmentManager, "popupTransaction")
-
-
         }
     }
 }
--- app/src/main/java/com/example/md3/addTransPopup.kt
+++ app/src/main/java/com/example/md3/addTransPopup.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.SpinnerAdapter
 import androidx.fragment.app.DialogFragment
@@ -17,16 +18,22 @@ class addTransPopup : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        //inflater
         val inflateView =  inflater.inflate(R.layout.trans_popup, container, false)
 
+        //menu tendina
         val transCat : Spinner = inflateView.findViewById(R.id.popupSpinner)
-
         var items = arrayOf<String>("Categoria1","Categoria2","Altro")
-
         val adapter: Any =
             ArrayAdapter<Any?>(requireContext(), android.R.layout.simple_spinner_dropdown_item, items)
-
         transCat.adapter = adapter as SpinnerAdapter?
+
+        //cancelButton
+        val cancel : Button = inflateView.findViewById(R.id.popupBack)
+        cancel.setOnClickListener{
+            dismiss()
+        }
+
 
         return inflateView
     }
