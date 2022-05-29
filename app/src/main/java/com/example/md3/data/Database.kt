241157d7fdd9cfdd69a80675db21fbcac5e8fed2
Coccoexe <74604365+Coccoexe@users.noreply.github.com>
2022-05-29 18:28:39 +0200

    delete database

--- app/src/main/java/com/example/md3/data/Database.kt
+++
@@ -1,9 +0,0 @@
-package com.example.md3.data
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [User::class], version = 1)
-abstract class Database : RoomDatabase(){
-    abstract fun userDao(): UserDao
-}
