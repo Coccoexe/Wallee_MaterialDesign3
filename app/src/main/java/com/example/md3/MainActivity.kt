e18a58ae3ca303ca4c4a8061e5968db5eb7b5198
Coccoexe <74604365+Coccoexe@users.noreply.github.com>
2022-05-29 16:12:31 +0200

    add navigation bottom

--- app/src/main/java/com/example/md3/MainActivity.kt
+++ app/src/main/java/com/example/md3/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.md3
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.color.DynamicColors
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigationController) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.navigationBar).setupWithNavController(navController)
 
     }
 }
