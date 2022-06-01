1a9a502a864f0968156bb6d3f3c048a5141f8cb3
Coccoexe <74604365+Coccoexe@users.noreply.github.com>
2022-06-01 09:56:32 +0200

    add controllo singola mail

--- app/src/main/java/com/example/md3/data/entity/User.kt
+++ app/src/main/java/com/example/md3/data/entity/User.kt
@@ -2,9 +2,10 @@ package com.example.md3.data.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(indices = [Index(value = ["userMail"], unique = true)])
 data class User(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
