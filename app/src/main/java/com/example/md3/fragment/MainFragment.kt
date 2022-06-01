42e6025779d1bed816079e9c197e6b3a725ceaf4
Coccoexe <74604365+Coccoexe@users.noreply.github.com>
2022-06-01 23:02:45 +0200

    add prima frenata

--- app/src/main/java/com/example/md3/fragment/MainFragment.kt
+++ app/src/main/java/com/example/md3/fragment/MainFragment.kt
@@ -1,8 +1,6 @@
 package com.example.md3.fragment
 
-import android.R.attr.bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,12 +8,16 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.md3.R
 import com.example.md3.data.entity.Transaction
 import com.example.md3.events.IActivityData
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -75,6 +77,23 @@ class MainFragment : Fragment() {
             lastImage.setImageResource(R.drawable.more)
         }
 
+        //incomeChart
+        val pieChart : PieChart = inflateView.findViewById(R.id.incomeChart)
+        val income : ArrayList<PieEntry> = ArrayList()
+        income.add(PieEntry(508f,"2016"))
+        income.add(PieEntry(600f,"2017"))
+        income.add(PieEntry(750f,"2018"))
+        income.add(PieEntry(420f,"2019"))
+        val pieDataSet : PieDataSet = PieDataSet(income,"Income")
+        pieDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+        pieDataSet.valueTextColor = Color.BLACK
+        pieDataSet.valueTextSize = 16f
+        val pieData : PieData = PieData(pieDataSet)
+        pieChart.data = pieData
+        pieChart.legend.isEnabled = false
+        pieChart.description.isEnabled = false
+        pieChart.centerText = "Income"
+        pieChart.animate()
 
         // Inflate the layout for this fragment
         return inflateView
