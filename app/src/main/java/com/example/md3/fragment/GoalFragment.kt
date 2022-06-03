af3364b074125906253e0f3323b797b2a7eef1fb
annaamarche <anna.marchesini99@gmail.com>
2022-06-03 17:14:04 +0200

    add FAB and CardGoalAdapter

--- app/src/main/java/com/example/md3/fragment/GoalFragment.kt
+++ app/src/main/java/com/example/md3/fragment/GoalFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.md3.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class GoalFragment : Fragment() {
@@ -14,8 +16,18 @@ class GoalFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_goal, container, false)
-    }
 
+        val inflateView = inflater.inflate(R.layout.fragment_goal, container, false)
+
+        val cardFAB : FloatingActionButton= inflateView.findViewById(R.id.cardFAB)
+
+        cardFAB.setOnClickListener {
+            Toast.makeText(this.context, "FAB is clicked", Toast.LENGTH_SHORT).show()
+            //val popup = AddCardPopup()
+            //popup.show(requireActivity().supportFragmentManager, "addCard")
+        }
+
+        // Inflate the layout for this fragment
+        return inflateView
+    }
 }
