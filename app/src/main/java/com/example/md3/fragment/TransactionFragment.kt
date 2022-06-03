433ea02237484a66645c04275306f8d5b5050a89
Coccoexe <74604365+Coccoexe@users.noreply.github.com>
2022-06-03 14:35:45 +0200

    add color transaction

--- app/src/main/java/com/example/md3/fragment/TransactionFragment.kt
+++ app/src/main/java/com/example/md3/fragment/TransactionFragment.kt
@@ -13,12 +13,12 @@ import com.example.md3.TransactionAdapter
 import com.example.md3.data.entity.Transaction
 import com.example.md3.events.IActivityData
 import com.google.android.material.color.DynamicColors
+import com.google.android.material.color.MaterialColors
 
 class TransactionFragment : Fragment() {
 
     private lateinit var activityData : IActivityData
     private lateinit var dataList : RecyclerView
-    private lateinit var adapter: TransactionAdapter
     private var transactionList : List<Transaction>? = null
     private lateinit var gridLayoutManager: GridLayoutManager
 
@@ -45,19 +45,38 @@ class TransactionFragment : Fragment() {
 
 
         val all : AppCompatImageView = inflateView.findViewById(R.id.allTransactions)
-        all.setImageResource(R.drawable.money_in_out_color)
         val positive : AppCompatImageView = inflateView.findViewById(R.id.positiveTransactions)
         val negative : AppCompatImageView = inflateView.findViewById(R.id.negativeTransactions)
+        val color : Int = MaterialColors.getColor(inflateView,com.google.android.material.R.attr.colorOnSurface)
 
+        //default
+        all.setImageResource(R.drawable.money_in_out_color)
+        positive.setColorFilter(color)
+        negative.setColorFilter(color)
+
+        //listener
         all.setOnClickListener{
+            //manage filter
+            all.clearColorFilter()
+            positive.setColorFilter(color)
+            negative.setColorFilter(color)
+
+            //manage image
             all.setImageResource(R.drawable.money_in_out_color)
             positive.setImageResource(R.drawable.money_in)
             negative.setImageResource(R.drawable.money_out)
+
             transactionList = activityData.getUserWithTransaction()
             setAdapter()
         }
 
         positive.setOnClickListener{
+            //manage filter
+            all.setColorFilter(color)
+            positive.clearColorFilter()
+            negative.setColorFilter(color)
+
+            //manage image
             all.setImageResource(R.drawable.money_in_out)
             positive.setImageResource(R.drawable.money_in_color)
             negative.setImageResource(R.drawable.money_out)
@@ -66,6 +85,12 @@ class TransactionFragment : Fragment() {
         }
 
         negative.setOnClickListener{
+            //manage filter
+            all.setColorFilter(color)
+            positive.setColorFilter(color)
+            negative.clearColorFilter()
+
+            //manage image
             all.setImageResource(R.drawable.money_in_out)
             positive.setImageResource(R.drawable.money_in)
             negative.setImageResource(R.drawable.money_out_color)
